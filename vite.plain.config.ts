633f380387938b90f36bee8e35d9627285b40e68
Exponential-Workload <cc@aaathats3as.com>
2023-11-01 05:18:39 +0100

    feat: ts ext

--- vite.plain.config.ts
+++ vite.plain.config.ts
@@ -2,11 +2,13 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import {copyFileSync} from 'fs';
+
+copyFileSync('dist/index.d.ts','vanilla/index.d.ts')
 
 export default defineConfig({
   build: {
     lib: {
-      // entry: resolve(__dirname, 'dist/index.js'),
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'UVC',
       fileName: 'index',
